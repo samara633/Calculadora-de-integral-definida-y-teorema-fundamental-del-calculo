4dfc90e93eccac7e42cb17827c54dee713f7cf8c
samara633 <samara20031012@gmail.com>
2023-11-20 13:09:41 -0500

    cambio teorema

--- backend/teorema.js
+++ backend/teorema.js
@@ -3,16 +3,20 @@ function calcularIntegral() {
     const x0 = parseFloat(document.getElementById('limite_inf').value);
     const x1 = parseFloat(document.getElementById('limite_sup').value);
 
+
     try {
         // Compilar y simplificar la función ingresada
         const compiledFunction = math.compile(math.simplify(funcion).toString());
+
 
         // Evaluar la función compilada en los puntos 'x0' y 'x1'
         const resultAtX0 = compiledFunction.evaluate({ x: x0 });
         const resultAtX1 = compiledFunction.evaluate({ x: x1 });
 
+
         // Calcular la integral numérica
         const result = math.simpson(compiledFunction, x0, x1);
+
 
         // Mostrar el resultado en un formato más legible (opcional)
         const formattedResult = math.format(result, { precision: 2 });
